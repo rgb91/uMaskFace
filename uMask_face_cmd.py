99af0bf489d3f04bff8d5e63c6ee31fb3d34c5b5
sanjaysaha1311 <sanjaysaha1311@yahoo.com>
2020-07-22 15:39:11 +0800

    working version of cmd line api

--- uMask_face_cmd.py
+++ uMask_face_cmd.py
@@ -1,24 +1,52 @@
 """
-Created by Sanjay at 7/21/2020
+::
 
-Feature: Enter feature name here
-Enter feature description here
+  uMask Face API Command Line
+
+  Usage:
+    uMask_face_cmd.py --new_img=<new_image_path> --old_img=<old_image_path> --out_img=<out_image_path> --out_json=<out_json_path> --uuid=<UUID> --grpid=<GroupID>
+
+  Options:
+    -h, --help                  Show this screen
+    --new_img=<filename>        Path to new image (.jpg, .jpeg, .png)
+    --old_img=<filename>        Path to old image (.jpg, .jpeg, .png)
+    --out_img=<filename>        Path to output image (.jpg, .jpeg, .png)
+    --out_json=<filename>       Path to JSON (.json)
+    --uuid=<uuid>               UUID
+    --grpid=<grpid>             Group ID
+    --version                   Show version
+
+
 """
+
 import os
 import time
 
 import dlib
 import numpy as np
+from PIL import Image
+from docopt import docopt
 from facemorpher import blender
 from facemorpher import plotter
 from facemorpher import warper
+from werkzeug.utils import secure_filename
 
 from env import PROJECT_HOME
 from utils import *
 
+UPLOADS_RESIZED_DIR = os.path.join(PROJECT_HOME, 'uploads_resized')
+OUTPUT_DIR = os.path.join(PROJECT_HOME, 'outputs')
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+MIN_HEIGHT = 500
+MIN_WIDTH = 500
+OUTPUT_HEIGHT = 500
+OUTPUT_WIDTH = 500
+p = "shape_predictor_68_face_landmarks.dat"
+REFERENCE_IMG_PATH = os.path.join(PROJECT_HOME, 'reference', 'reference.jpg')
 
-def average_face(imgpaths, dest_filename=None, width=500, height=500, background='black',
-                 blur_edges=False, out_filename='result.png'):
+
+def average_face(imgpaths, width=500, height=500, background='black',
+                 blur_edges=False, out_filename='result.jpg'):
     size = (height, width)
 
     images = []
@@ -32,13 +60,15 @@ def average_face(imgpaths, dest_filename=None, width=500, height=500, background
     if len(images) == 0:
         raise FileNotFoundError('Could not find any valid images. Supported formats are .jpg, .png, .jpeg')
 
-    if dest_filename is not None:
-        dest_img, dest_points = load_image_points(dest_filename, size)
-        if dest_img is None or dest_points is None:
-            raise Exception('No face or detected face points in dest img: ' + dest_filename)
-    else:
-        dest_img = np.zeros(images[0].shape, np.uint8)
-        dest_points = locator.average_points(point_set)
+    dest_img, dest_points = load_image_points(REFERENCE_IMG_PATH, size)
+
+    # if dest_filename is not None:
+    #     dest_img, dest_points = load_image_points(dest_filename, size)
+    #     if dest_img is None or dest_points is None:
+    #         raise Exception('No face or detected face points in dest img: ' + dest_filename)
+    # else:
+    #     dest_img = np.zeros(images[0].shape, np.uint8)
+    #     dest_points = locator.average_points(point_set)
 
     num_images = len(images)
     result_images = np.zeros(images[0].shape, np.float32)
@@ -67,45 +97,56 @@ def average_face(imgpaths, dest_filename=None, width=500, height=500, background
     plt.save(dest_img)
 
 
-def controller(new_image_path, old_image_path, uuid, grpid):
-    input_filename = os.path.basename(new_image_path)
-    out_filename = str(input_filename.split('_')[0]) + '.jpg'
-    out_img_path = os.path.join(OUTPUT_DIRECTORY, out_filename)
-    average_face([new_image_path, old_image_path], background='average', out_filename=out_img_path)
-    landmarks, msg = get_landmarks(new_image_path, detector, predictor)
-    return prepare_output(out_img_path, landmarks, uuid, grpid, msg)
+def main():
+    # Read input images
+    new_im = Image.open(new_img_path)
+    old_im = Image.open(old_img_path)
 
-
-OUTPUT_DIRECTORY = os.path.join(PROJECT_HOME, 'outputs')
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
-p = "shape_predictor_68_face_landmarks.dat"
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(p)
-
-if __name__ == '__main__':
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    new_filename = timestamp + '_' + secure_filename(new_file.filename)
-    old_filename = timestamp + '_' + secure_filename(old_file.filename)
-
-    new_file.save(os.path.join('tmp', new_filename))
-    old_file.save(os.path.join('tmp', old_filename))
-    new_im = Image.open(os.path.join('tmp', new_filename))
-    old_im = Image.open(os.path.join('tmp', old_filename))
-
+    # Verify size of the input images
     new_file_width, new_file_height = new_im.size
     old_file_width, old_file_height = old_im.size
-    if new_file_width + new_file_height < 1000 or old_file_width + old_file_height < 1000:
-        return prepare_output(None, None, uuid, grpid, 'Both images should be at least of size: (500, 500).')
 
-    new_im_resized = _resize(new_im, 500, 500)  # new_im.resize((500, 500))
-    old_im_resized = _resize(old_im, 500, 500)  # old_im.resize((500, 500))
+    if new_file_width + new_file_height < MIN_HEIGHT + MIN_WIDTH or old_file_width + old_file_height < MIN_HEIGHT + MIN_WIDTH:
+        print('ERROR: Both images should be at least of size: (500, 500).')
 
-    new_dir = os.path.join(app.config['UPLOAD_FOLDER'], grpid)
+    # Resize input images
+    new_im_resized = PIL_Image_resize(new_im, OUTPUT_WIDTH + 100, OUTPUT_HEIGHT + 100)
+    old_im_resized = PIL_Image_resize(old_im, OUTPUT_WIDTH + 100, OUTPUT_HEIGHT + 100)
+
+    # Save the resized input images
+    new_dir = os.path.join(UPLOADS_RESIZED_DIR, grpid)
     if not os.path.exists(new_dir):
         os.mkdir(new_dir)
-    new_filepath = os.path.join(new_dir, new_filename)
-    old_filepath = os.path.join(new_dir, old_filename)
-    new_im_resized.save(new_filepath)
-    old_im_resized.save(old_filepath)
-    json_data = controller(new_filepath, old_filepath, uuid, grpid)
-    return json_data
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    new_img_resized_name = timestamp + '_' + os.path.basename(new_img_path)
+    old_img_resized_name = timestamp + '_' + os.path.basename(old_img_path)
+    new_img_resized_path = os.path.join(new_dir, new_img_resized_name)
+    old_img_resized_path = os.path.join(new_dir, old_img_resized_name)
+    new_im_resized.save(new_img_resized_path)
+    old_im_resized.save(old_img_resized_path)
+
+    # Process input face images
+    try:
+        average_face([new_img_resized_path, old_img_resized_path], background='average', out_filename=out_img_path)
+    except Exception as e:
+        print(e)
+        return
+    landmarks, msg = get_landmarks(new_img_resized_path, detector, predictor)
+    out_json = get_output_json(landmarks, uuid, grpid, msg)
+    with open(out_json_path, 'w') as outfile:
+        json.dump(out_json, outfile)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='uMask Face Command line 1.0')
+    new_img_path, old_img_path = args['--new_img'], args['--old_img']
+    out_img_path, out_json_path = args['--out_img'], args['--out_json']
+    uuid, grpid = args['--uuid'], args['--grpid']
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(p)
+
+    main()
+    print('SUCCESS: Output face saved at', out_img_path)
+
+# python uMask_face_cmd.py --new_img "./morph_images/group04/new.jpg" --old_img "./morph_images/group04/old.jpg" --out_img "./morph_images/group04/out.jpg" --out_json "./morph_images/group04/out.json" --uuid 12345 --grpid g04
--- utils.py
+++ utils.py
@@ -5,71 +5,56 @@ Feature: Enter feature name here
 Enter feature description here
 """
 import traceback
-
+import json
+import base64
 import cv2
 from PIL import ExifTags
 from facemorpher import locator, aligner
 from imutils import face_utils
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(filename, allowed_extensions):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
-def prepare_output(img_path: str, landmarks, uuid: str, grpid: str, msg: str):
+def get_output_json(landmarks, uuid: str, grpid: str, msg: str):
     """
     Prepare JSON data to return.
-    :param msg:
-    :param img: Stylized image (numpy array 3D)
+    :param msg: String
     :param landmarks: List of Landmarks of input face (numpy array)
-    :param filename: name of the output file
     :param uuid: String, Unique Identifier
     :param grpid: String, Group Identifier
     :return: JSON String (Base64 image, landmarks)
     """
-    if landmarks is None or len(img_path) < 1:
+    if landmarks is None:
         return json.dumps({
             "uuid": uuid,
             "grpid": grpid,
             "msg": msg,
-            "facial_features": '',
-            "stylized_image": ''
+            "facial_features": ''
         })
-    # else:
-    #     filepath = os.path.join(OUTPUT_DIRECTORY, filename)
-    #     img = img.numpy()
-    #     tf.keras.preprocessing.image.save_img(filepath, img, data_format=None, file_format=None, scale=True)
-
-    # img = tf.cast(img, tf.uint8)
-    # img = tf.image.encode_png(img, compression=-1, name=None)
-    # tf.io.write_file(filepath, img, name=None)
-
-    # cv2.imwrite(filepath, img)
-
-    with open(img_path, "rb") as image_file:
-        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
     out_json_data = {
         "uuid": uuid,
         "grpid": grpid,
         "msg": msg,
-        "facial_features": landmarks.tolist(),
-        "stylized_image": encoded_image
+        "facial_features": landmarks.tolist()
     }
     return json.dumps(out_json_data)
 
 
 def PIL_Image_resize(im, width, height):
     try:
-        for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation] == 'Orientation': break
-        exif = dict(im._getexif().items())
+        if im._getexif() is not None:
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation] == 'Orientation': break
+            exif = dict(im._getexif().items())
 
-        if exif[orientation] == 3:
-            im = im.rotate(180, expand=True)
-        elif exif[orientation] == 6:
-            im = im.rotate(270, expand=True)
-        elif exif[orientation] == 8:
-            im = im.rotate(90, expand=True)
+            if exif[orientation] == 3:
+                im = im.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                im = im.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                im = im.rotate(90, expand=True)
 
         # image.thumbnail((width, heght), Image.ANTIALIAS)
         im_resized = im.resize((width, height))
